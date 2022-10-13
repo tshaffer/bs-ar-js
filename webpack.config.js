203df565c589f5649998598d8d1c467151b70d56
Ted Shaffer <ted@brightsign.biz>
2022-10-13 05:17:29 -0700

    further improvements

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,26 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const path = require('path');
 var webpack = require('webpack');
+
+// TEDTODO - these should be environment variables, command line arguments
 var appBrightSignMock = '/Users/tedshaffer/Documents/Projects/bs-ar-js/src/mock';
+var target = 'brightsign';
+// var target = 'dev';
 
-module.exports = {
-  entry: './src/index.tsx',
-  output: {
-    libraryTarget: 'umd',
-    publicPath: './build/',
-    filename: 'bundle.js',
-    path: __dirname + '/build'
-  },
-  mode: 'development',
+let plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify('development'),
+  }),
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify('production')
+    }
+  })
+];
+var externals = {};
 
-  target: 'electron-renderer',
-
-  resolve: {
-    // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: ['.ts', '.tsx', '.js', '.json']
-  },
-
-  devtool: 'inline-source-map',
-
-  // TEDTODO - plugins, node, externals
-  
-  // MAC version: plugins, node?, externals
-  plugins: [
+if (target !== 'brightsign') {
+  plugins.push(
     new webpack.NormalModuleReplacementPlugin(/^@brightsign.*$/, (resource) => {
       resource.request = resource.request.replace(/^@brightsign\/assetpool$/, path.resolve(appBrightSignMock, 'assetpool'));
       resource.request = resource.request.replace(/^@brightsign\/assetpoolfetcher$/, path.resolve(appBrightSignMock, 'assetpoolfetcher'));
@@ -49,38 +44,51 @@ module.exports = {
       resource.request = resource.request.replace(/^@brightsign\/videoinput$/, path.resolve(appBrightSignMock, 'videoinput'));
       resource.request = resource.request.replace(/^@brightsign\/videomodeconfiguration$/, path.resolve(appBrightSignMock, 'videomodeconfiguration'));
       resource.request = resource.request.replace(/^@brightsign\/videooutput$/, path.resolve(appBrightSignMock, 'videooutput'));
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development'),
-    }),
-    new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
-      }
     })
-  ].filter(Boolean),
+  );
+} else {
+  externals = {
+    BSDeviceInfo: 'BSDeviceInfo',
+    '@brightsign/registry': 'commonjs @brightsign/registry',
+    '@brightsign/systemtime': 'commonjs @brightsign/systemtime',
+    '@brightsign/networkconfiguration': 'commonjs @brightsign/networkconfiguration',
+    '@brightsign/videooutput': 'commonjs @brightsign/videooutput',
+    '@brightsign/screenshot': 'commonjs @brightsign/screenshot',
+
+    '@brightsign/videomodeconfiguration': 'commonjs @brightsign/videomodeconfiguration',
+    '@brightsign/videoinput': 'commonjs @brightsign/videoinput',
+    '@brightsign/system': 'commonjs @brightsign/system',
+
+    '@brightsign/hostconfiguration': 'commonjs @brightsign/hostconfiguration',
+    '@brightsign/networkdiagnostics': 'commonjs @brightsign/networkdiagnostics',
+    'core-js/fn/object/assign': 'commonjs core-js/fn/object/assign',
+  };
+}
+
+module.exports = {
+  entry: './src/index.tsx',
+  output: {
+    libraryTarget: 'umd',
+    publicPath: './build/',
+    filename: 'bundle.js',
+    path: __dirname + '/build'
+  },
+  mode: 'development',
+
+  target: 'electron-renderer',
+
+  resolve: {
+    // Add '.ts' and '.tsx' as resolvable extensions.
+    extensions: ['.ts', '.tsx', '.js', '.json']
+  },
+
+  devtool: 'inline-source-map',
+
+  plugins: plugins,
 
   node: {}, // possibly / probably unnecessary
 
-  externals: {},
-
-  // MAC version: plugins, node?, externals
-  // externals: {
-  //   BSDeviceInfo: 'BSDeviceInfo',
-  //   '@brightsign/registry': 'commonjs @brightsign/registry',
-  //   '@brightsign/systemtime': 'commonjs @brightsign/systemtime',
-  //   '@brightsign/networkconfiguration': 'commonjs @brightsign/networkconfiguration',
-  //   '@brightsign/videooutput': 'commonjs @brightsign/videooutput',
-  //   '@brightsign/screenshot': 'commonjs @brightsign/screenshot',
-
-  //   '@brightsign/videomodeconfiguration': 'commonjs @brightsign/videomodeconfiguration',
-  //   '@brightsign/videoinput': 'commonjs @brightsign/videoinput',
-  //   '@brightsign/system': 'commonjs @brightsign/system',
-
-  //   '@brightsign/hostconfiguration': 'commonjs @brightsign/hostconfiguration',
-  //   '@brightsign/networkdiagnostics': 'commonjs @brightsign/networkdiagnostics',
-  //   'core-js/fn/object/assign': 'commonjs core-js/fn/object/assign',
-  // },
+  externals: externals,
 
   module: {
     rules: [
